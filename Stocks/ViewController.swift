0f12dc232ad0e99e91667e7529e704a89206ca55
likils <n.kolesnikov@hotmail.com>
2021-04-09 17:33:28 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by likils on 09.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
